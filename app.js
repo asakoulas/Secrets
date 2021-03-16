53cec3ead07f62911269e46980038623fb1ddaf9
Alexandre Sakoulas <asakoulas@Alexandres-MacBook-Pro.local>
2021-03-15 21:31:53 -0400

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-var encrypt = require('mongoose-encryption');
+var md5 = require('md5');
 
 const app = express();
 
@@ -22,8 +22,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
@@ -44,7 +42,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save(function (err) {
@@ -58,7 +56,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const userName = req.body.username;
-  const passWord = req.body.password;
+  const passWord = md5(req.body.password);
 
   User.findOne({ email: userName }, function (err, foundUser) {
     if (err) {
